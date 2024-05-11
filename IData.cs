307cf4a2873af544dc43d3dd4614837f06079937
Arman Arjmand <arjmand.arman81@gmail.com>
2024-05-11 13:26:13 +0330

    Finished, could do some interface stuff

---
+++ IData.cs
@@ -0,0 +1,183 @@
+using Microsoft.VisualBasic.FileIO;
+
+namespace MyData;
+
+public interface IData
+{    
+
+    public static string[][] CSVDataRead_Function(string filePath_String)
+    {
+    
+        return GetDataCSVPrivate_Function(filePath_String);
+    
+    }
+
+    public static int[] CastStringToInt_Function(string[][] initialData_StringArray2D, int column_Int)
+    {
+
+        return StringToIntPrivate_Function(initialData_StringArray2D, column_Int);
+    
+    }
+
+    public static double[] Parse2Double_Function(string[][] csvData_StringArray2D, int columnNumber_Int)
+    {
+
+        return ParseCSV2DoublePrivate_Function(csvData_StringArray2D, columnNumber_Int);
+
+    }
+
+    public static int[] Parse2Int_Function(string[][] csvData_StringArray2D, int columnNumber_Int)
+    {
+
+        return ParseCSV2IntPrivate_Function(csvData_StringArray2D, columnNumber_Int);
+
+    }
+
+    private static double[] ParseCSV2DoublePrivate_Function(string[][] csvData_StringArray2D, int columnNumber_Int)
+    {
+
+        double[] targetColumn_StringArray = new double[csvData_StringArray2D.Length];
+
+        for (int row_Int = 1; row_Int < csvData_StringArray2D.Length-1; row_Int++)
+        {
+
+            try
+            {
+                
+                targetColumn_StringArray[row_Int-1] = double.Parse(csvData_StringArray2D[row_Int][columnNumber_Int]);
+                
+            }
+            catch (System.Exception parsing_Exception)
+            {
+                
+                System.Console.WriteLine(parsing_Exception);
+                
+            }
+            
+        }
+
+        return targetColumn_StringArray;
+
+    }
+
+    private static int[] ParseCSV2IntPrivate_Function(string[][] csvData_StringArray2D, int columnNumber_Int)
+    {
+
+        int[] targetColumn_StringArray = new int[csvData_StringArray2D.Length];
+
+        for (int row_Int = 0; row_Int < csvData_StringArray2D.Length; row_Int++)
+        {
+
+            _ = int.TryParse(csvData_StringArray2D[row_Int][columnNumber_Int], out targetColumn_StringArray[row_Int]);
+            
+        }
+
+        return targetColumn_StringArray;
+
+    }
+
+    private static string[][] GetDataCSVPrivate_Function(string filePath_String)
+    {
+
+        try
+        {
+
+            int length_Int = File.ReadAllLines(filePath_String).Length;
+
+            string[][] csvData_StringArray2D = new string[length_Int][];
+
+            for (int row_Int = 0; row_Int < length_Int-1; row_Int++)
+            {
+
+                csvData_StringArray2D[row_Int] = File.ReadAllLines(filePath_String)[row_Int+1].Split(',');
+                
+            }
+
+            return csvData_StringArray2D;
+
+        }
+        catch (System.Exception readCSV_Exception)
+        {
+            
+            System.Console.WriteLine(readCSV_Exception);
+
+        }
+        
+        return [];
+
+        //my way?:
+
+        // try
+        // {
+
+        //     string[][] csvData_StringArray2D = [];
+
+        //     if(!File.Exists(filePath_String))
+        //     {
+
+        //         System.Console.WriteLine("File Not Found!");
+
+        //     }
+
+        //     if(File.Exists("local.csv"))File.Delete("local.csv");
+
+        //     File.Copy(filePath_String,"local.csv");
+
+        //     TextFieldParser csvFileReader_TextFieldParser = new("local.csv");
+
+        //     csvFileReader_TextFieldParser.SetDelimiters(",");
+
+        //     int rowCount_Int = 0;
+
+        //     string[]? readLine;
+
+        //     while ((readLine = csvFileReader_TextFieldParser.ReadFields()) != null)
+        //     {
+
+        //         csvData_StringArray2D[rowCount_Int] = readLine;
+
+        //         rowCount_Int++;
+                
+        //     }
+
+        //     csvFileReader_TextFieldParser.Dispose();
+
+        //     return csvData_StringArray2D;
+            
+        // }
+        // catch (System.Exception fieldParser_Exception)
+        // {
+
+            
+        //     System.Console.WriteLine(fieldParser_Exception);
+
+        // }
+
+        // return [];
+
+    }
+
+    private static int[] StringToIntPrivate_Function(string[][] initialData_StringArray2D, int column_Int)
+    {
+
+        int[] processedData_IntArray = new int[initialData_StringArray2D.Length];
+
+        for (int rowCount_Int = 0; rowCount_Int < initialData_StringArray2D.Length; rowCount_Int++)
+        {
+
+            if(!int.TryParse(initialData_StringArray2D[rowCount_Int][column_Int], out processedData_IntArray[rowCount_Int]))
+            {
+
+                System.Console.WriteLine("Error While Parsing To Integer!");
+
+                return [];
+
+            }
+            
+        }
+
+        return processedData_IntArray;
+    
+    }
+
+} 
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using MyData;
+using MathNet.Numerics.Statistics;
+using MathNet.Numerics;
+
+string[][] csvRawData_String2DArray = IData.CSVDataRead_Function("weight-height.csv");
+
+double[][] csvData_Double2DArray =
+[
+    [.. IData.Parse2Double_Function(csvRawData_String2DArray, 1).ToList().Order()],
+    [.. IData.Parse2Double_Function(csvRawData_String2DArray, 2).ToList().Order()],
+];
+
+IEnumerable<double> height_DoubleIEnumerable = csvData_Double2DArray[0].AsEnumerable();
+
+IEnumerable<double> weight_DoubleIEnumerable = csvData_Double2DArray[1].AsEnumerable();
+
+Histogram histogram1 = new(height_DoubleIEnumerable,12);
+
+Histogram histogram2 = new(weight_DoubleIEnumerable,12);
+
+Console.Clear();
+
+System.Console.WriteLine("Height Histogram Buckets:");
+
+Console.WriteLine(histogram1);
+
+System.Console.WriteLine("--------------------------------\n\nWeight Histogram Buckets:");
+
+Console.WriteLine(histogram2);
+
+System.Console.WriteLine("--------------------------------\n\nBest Fitted Line:");
+
+(double c_Double,double d_Double) = Fit.Line(csvData_Double2DArray[0],csvData_Double2DArray[1]);
+
+System.Console.WriteLine($"Intercept = {c_Double} & Slope = {d_Double}");
+
+System.Console.WriteLine($"Line: {c_Double} + {d_Double}*t");
+
+System.Console.WriteLine("--------------------------------\n\nBest Fitted Parabole:");
+
+double[] bestFittingP_DoubleArray = Fit.Polynomial(csvData_Double2DArray[0],csvData_Double2DArray[1],2);
+
+System.Console.WriteLine($"Constant = {bestFittingP_DoubleArray[0]} & 1st Order = {bestFittingP_DoubleArray[1]} & 2nd Order = {bestFittingP_DoubleArray[2]}");
+
+System.Console.WriteLine($"Parabole: {bestFittingP_DoubleArray[0]} + {bestFittingP_DoubleArray[1]}*t + {bestFittingP_DoubleArray[2]}*t^2");
